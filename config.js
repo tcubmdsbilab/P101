d54d7c69877c4f1ad3a8839c978edaaf40b6f5b0
tcubmdsbilab <tcubmdsbilab@gmail.com>
2026-05-05 12:14:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // 請複製此檔案為 config.js，並填入您的 Supabase 專案資訊。
 // 注意：config.js 若包含真實 anon key，可上傳到 GitHub Pages，但不可放 service_role key。
 window.SBI_CONFIG = {
-  SUPABASE_URL: "https://mfljkyvdadxlrbxlboce.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://mfljkyvdadxlrbxlboce.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mbGpreXZkYWR4bHJieGxib2NlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MTQwMDUsImV4cCI6MjA4MDM5MDAwNX0.Z4OeacVpO8yM1d1uOWZ6jU2Gl7wgEbhXvAFSqF5pBRs",
   COUNTER_RPC: "p101_increment_counter"
 };
