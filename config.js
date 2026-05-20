06c95f6a54a4a85ee9b6cfdea929a8f6a699b76b
tcubmdsbilab <tcubmdsbilab@gmail.com>
2026-05-20 10:37:59 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // 請複製此檔案為 config.js，並填入您的 Supabase 專案資訊。
 // 注意：config.js 可放 anon public key，但絕對不可放 service_role key。
 window.P101_CONFIG = {
-  SUPABASE_URL: "https://mfljkyvdadxlrbxlboce.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://mfljkyvdadxlrbxlboce.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mbGpreXZkYWR4bHJieGxib2NlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MTQwMDUsImV4cCI6MjA4MDM5MDAwNX0.Z4OeacVpO8yM1d1uOWZ6jU2Gl7wgEbhXvAFSqF5pBRs",
   COUNTER_FUNCTION: "P101_increment_counter"
 };
